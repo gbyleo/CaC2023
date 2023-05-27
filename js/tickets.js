58472f3bead9ad23d083e36372892cbb4aac28ee
Leonardo Seivetti <leogs22@gmail.com>
2023-05-27 00:15:19 -0300

    fn_borrar_y_resumen
    creacion de las funcion borrar que blanquea los campos del formulario
    creacion de la funcion resumen que da el resultado total a pagar por los tickets segun categoria y cantidad

---
+++ js/tickets.js
@@ -0,0 +1,31 @@
+const valor = 200;
+
+function borrar() {
+    validationCustom01_nom.value = '';
+    validationCustom01_ap.value = '';
+    validationCustom01_correo.value = '';
+    vc_cantidad.value = 0;
+    categoria.value = 80;
+    precioPagar.innerText = 0;
+}
+
+
+function resumen() {
+    let resultado = 0;
+    const apunta_resultado = document.getElementById("precioPagar")
+    let categoria_elegida = parseInt(categoria.value);
+    resultado = vc_cantidad.value * (valor - ((categoria_elegida * valor) / 100));
+    apunta_resultado.innerText = resultado;
+}
+
+
+/*}
+Email.value = '';
+Cantidad.value = '';
+Categoria.value = '';
+valorTicketDesc = 0;
+totalSpan.innerText = 0;
+form.classList.add('needs-validation')
+form.classList.remove('was-validated')
+} */
+
